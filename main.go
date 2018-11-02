06a7023729dba3aaf2a8da569a8de84589c1e71b
fess932 <fess932@gmail.com>
2018-11-02 14:59:49 +0300

    asdf

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	r := chi.NewRouter()
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("welcome"))
+		w.Write([]byte("OH YES"))
 	})
 	http.ListenAndServe(":3000", r)
 }
